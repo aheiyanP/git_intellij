46155ddd732fd92aa54aaa1009093e4cf03cacd5
170202102575 <15363245680@qq.com>
2020-08-18 08:10:58 +0800

    intellij git

--- gitdemo/src/com/zx/git/gitdemo.java
+++ gitdemo/src/com/zx/git/gitdemo.java
@@ -4,4 +4,8 @@ public class gitdemo {
     public static void main(String[] args) {
         System.out.println("git project");
     }
+    public void test(){
+        int i= 10;
+        i += 10;
+    }
 }
