e5011103382f6fbb1afc48a6a042f98e802c59af
170202102575 <15363245680@qq.com>
2020-08-18 08:19:45 +0800

    clone push

--- gitdemo/src/com/zx/git/gitdemo.java
+++ gitdemo/src/com/zx/git/gitdemo.java
@@ -4,8 +4,10 @@ public class gitdemo {
     public static void main(String[] args) {
         System.out.println("git project");
     }
+
     public void test(){
         int i= 10;
         i += 10;
+        System.out.println("git test");
     }
 }
